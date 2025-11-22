4fb63cc56f833558a14250a33576034b32ae639d
tanuja-kotla <thanujakotla2007@gmail.com>
2025-11-22 14:31:30 +0530

    Add files via upload

---
+++ search an element in array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>  
+int main()    
+{
+	int array[100],search,c,n;
+	printf("enter number of elements in array\n");
+	scanf("%d",&n);
+	printf("enter %d integer(s)\n",n);
+	for(c==0;c<n;c++)
+	scanf("%d",&array[c]);
+	
+	printf("enter a number to search\n");
+	scanf("%d",&search);
+	for(c=0;c<n;c++)
+	{
+		if(array[c]==search)
+		{
+			printf("%d is prensent at location %d.\n",search,c+1);
+			break;
+		}
+	}
+	if(c==n)
+	printf("%d isnt present in the array.\n",search);
+	return 0;
+	}    
