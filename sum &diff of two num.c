ea8387c49f3df25bf6ab18598878664105e9dff1
tanuja-kotla <thanujakotla2007@gmail.com>
2025-11-22 14:00:30 +0530

    Add files via upload

---
+++ sum &diff of two num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	float c,d;
+	scanf("%d%d",&a,&b);
+	scanf("%f%f",&c,&d);
+	printf("%d%d\n",(a+b),(a-b));
+	printf("%0.1f%0.f",(c+d),(c-d));
+	return 0;
+}
